ab12864865fff40d58ca8e7fa6169ba04d3b1012
augusto souza <131267188+augusto-s0uza@users.noreply.github.com>
2024-11-15 17:29:27 -0300

    arquivos da atividade 04

---
+++ atividade04/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Counter from "./Contador";
+
+function App() {
+  return (
+    <div>
+      <h1>Contador:</h1>
+      <Counter />
+    </div>
+    
+  
+  );
+}
+
+export default App;
